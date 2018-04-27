7f30e136c84b5676f5e072da29fe120c721c213b
吳宜珈 <c14031081@mail.ncku.edu.tw>
2018-04-27 11:13:12 +0800

    QTableWidget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QHeaderView>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //QHeaderView::setResizeMode ( QHeaderView::Stretch );
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 }
 
 MainWindow::~MainWindow()
